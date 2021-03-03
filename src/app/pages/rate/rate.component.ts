feeabd9ba6b707c4a75120bad78a1e83de450df8
DomoPp <phuditdd@gmail.com>
2021-03-03 12:49:47 +0700

    no message

--- src/app/pages/rate/rate.component.ts
+++ src/app/pages/rate/rate.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-rate',
   templateUrl: './rate.component.html',
-  styleUrls: ['./rate.component.css']
+  styleUrls: ['../chapter1/chapter1.component.css']
 })
 export class RateComponent implements OnInit {
+
+  checkr='0'
+  rate=0
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  a(){}
 
 }
